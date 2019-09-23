7b6181fb5414d2630b053588315931d675cf608e
daveywilkie <davey.wilkie@gmail.com>
2019-09-22 14:46:53 -1000

    p1 WIP

--- p1/script.js
+++ p1/script.js
@@ -1,4 +1,35 @@
-// The Vue instance
+let makeGuess = function(secretNumber, guessNumber, guess, message) {
+  guessNumber = guessNumber + 1;
+  var guessInt = parseFloat(guess, 10);
+
+  if (guessNumber >= 10) {
+    message = 'Out of guesses!'
+  }
+
+  if (typeof guessInt === 'number' && isNaN(guessInt) ||
+      typeof guessInt !== 'number' || Math.round(guessInt) != guessInt) {
+    message = 'Please enter an integer.';
+  }
+  else if (guessInt < secretNumber) {
+    message = 'Your guess was too low.';
+  }
+  else if (guessInt > secretNumber) {
+    message = 'Your guess was too high.';
+  }
+  else if (guessInt == secretNumber) {
+    message = 'You guessed right!';
+  }
+
+  console.log(guessNumber);
+};
+
 let app = new Vue({
-  el: "#app"
+  el: "#app",
+  data: {
+    secretNumber: Math.round(Math.random() * 100),
+    guessNumber: 0,
+    guess: null,
+    makeGuess: makeGuess,
+    message: ""
+  }
 });
