04b7be654b566821bd1759d4bf63fb9707701f94
daveywilkie <davey.wilkie@gmail.com>
2019-09-24 15:03:33 -1000

    P1

--- p1/script.js
+++ p1/script.js
@@ -1,35 +1,43 @@
-let makeGuess = function(secretNumber, guessNumber, guess, message) {
-  guessNumber = guessNumber + 1;
-  var guessInt = parseFloat(guess, 10);
-
-  if (guessNumber >= 10) {
-    message = 'Out of guesses!'
-  }
-
-  if (typeof guessInt === 'number' && isNaN(guessInt) ||
-      typeof guessInt !== 'number' || Math.round(guessInt) != guessInt) {
-    message = 'Please enter an integer.';
-  }
-  else if (guessInt < secretNumber) {
-    message = 'Your guess was too low.';
-  }
-  else if (guessInt > secretNumber) {
-    message = 'Your guess was too high.';
-  }
-  else if (guessInt == secretNumber) {
-    message = 'You guessed right!';
-  }
-
-  console.log(guessNumber);
-};
-
 let app = new Vue({
   el: "#app",
   data: {
     secretNumber: Math.round(Math.random() * 100),
     guessNumber: 0,
     guess: null,
-    makeGuess: makeGuess,
-    message: ""
+    message: "",
+    won: false
+  },
+  methods: {
+    takeGuess() {
+      console.log(this.secretNumber);
+      this.guessNumber = this.guessNumber + 1;
+      var guessInt = parseFloat(this.guess, 10);
+
+      if (this.guessNumber >= 10) {
+        this.message = 'Out of guesses!'
+      };
+
+      if (typeof guessInt === 'number' && isNaN(guessInt) ||
+          typeof guessInt !== 'number' || Math.round(guessInt) != guessInt) {
+        this.message = 'Please enter an integer.';
+      }
+      else if (guessInt < this.secretNumber) {
+        this.message = 'Your guess was too low.';
+      }
+      else if (guessInt > this.secretNumber) {
+        this.message = 'Your guess was too high.';
+      }
+      else if (guessInt == this.secretNumber) {
+        this.message = 'You guessed right!';
+        this.won = true;
+      };
+    },
+
+    playAgain() {
+      this.won = false;
+      this.secretNumber = Math.round(Math.random() * 100);
+      this.guessNumber = 0;
+      this.message = "";
+    }
   }
 });
