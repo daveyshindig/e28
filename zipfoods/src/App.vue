9a85ac8daa1c59624e67edbf59cad38a256cf44a
daveywilkie <davey.wilkie@gmail.com>
2019-11-05 08:41:13 -1000

    zipfoods named routes

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -1,40 +1,40 @@
 <template>
-    <div id='app'>
-        <img id='logo' alt='ZipFoods logo' src='./assets/images/zipfoods-logo.png' />
+  <div id="app">
+    <img id="logo" alt="ZipFoods logo" src="./assets/images/zipfoods-logo.png" />
 
-        <nav>
-            <ul>
-                <li v-for='link in links' :key='link'>
-                    <router-link exact :to='paths[link]'>{{ link }}</router-link>
-                </li>
-            </ul>
-        </nav>
+    <nav>
+      <ul>
+        <li v-for="link in links" :key="link">
+          <router-link :to="{ name: link }" exact>{{ link }}</router-link>
+        </li>
+      </ul>
+    </nav>
 
-        <router-view></router-view>
-    </div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import { products } from './products.js';
+import { products } from "./products.js";
 
 export default {
-    name: 'app',
-    components: {},
-    data: function() {
-        return {
-            products: products,
-            // src/App.vue data properties
-            links: ['home', 'products', 'categories'],
-            paths: {
-                home: '/',
-                products: '/products',
-                categories: '/categories'
-            }
-        };
-    }
+  name: "app",
+  components: {},
+  data: function() {
+    return {
+      products: products,
+      // src/App.vue data properties
+      links: ["home", "products", "categories"],
+      paths: {
+        home: "/",
+        products: "/products",
+        categories: "/categories"
+      }
+    };
+  }
 };
 </script>
 
 <style lang='scss'>
-@import './assets/css/zipfoods.scss';
+@import "./assets/css/zipfoods.scss";
 </style>
