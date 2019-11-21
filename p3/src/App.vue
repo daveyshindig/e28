071f860f3624111112b531dd5ba0e4a7aee1287d
daveywilkie <davey.wilkie@gmail.com>
2019-11-21 13:06:36 -1000

    p3 init commit

---
+++ p3/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  el: "#app",
+  name: "808MIX",
+  components: {
+    navbar: Navbar
+  }
+};
+</script>
+
+<style>
+#app {
+}
+</style>
---
+++ p3/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-navbar type="dark">
+    <b-navbar-brand href="#">
+      <img src="https://placekitten.com/g/30/30" class="d-inline-block align-top" alt="Kitten" />
+      808MIX
+    </b-navbar-brand>
+    <b-navbar-nav>
+      <b-nav-item to="home">Home</b-nav-item>
+      <b-nav-item to="about">About</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {};
+  },
+  template: "#navbar"
+};
+</script>
